5bb28766bed61982942dd59a98f26a4a33356b54
zhou <zhoutjcc@msn.cn>
2022-01-25 13:56:17 +0800

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by zhou on 2022/1/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
